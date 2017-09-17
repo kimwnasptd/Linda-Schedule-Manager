c2d37a378b8edb2393c65ac0f0aea6a76406e4d4
Kimonas <kimwnasptd@gmail.com>
2017-09-17 22:22:00 +0300

    Cleaned the structure and removed unneeded files

--- source/test.py
+++
@@ -1,8 +0,0 @@
-import pyttsx3
-
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-for voice in voices:
-    engine.setProperty('voice',voice.id)
-    engine.say('Assignment added successfully')
-    engine.runAndWait()
